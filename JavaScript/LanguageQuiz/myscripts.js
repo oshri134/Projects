859677c72b9e2c0694c4f9d9ff7d2f79a1c5cf84
oshri134 <54857364+oshri134@users.noreply.github.com>
2021-07-22 17:39:21 +0300

    js

---
+++ JavaScript/LanguageQuiz/myscripts.js
@@ -0,0 +1,335 @@
+const Color= [
+
+
+    {
+       "id":1,
+      word:"red",
+      options:['Pink','Blau','Grün','Rot'],
+      img:'https://www.or-taas.co.il/wp-content/uploads/2014/02/red-06.jpg',
+      rightAnswer:"Rot"
+   
+   
+    },
+    {
+       "id":2,
+    word:"blue",
+      options:['Pink','Blau','Grün','Rot'],
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Solid_blue.svg/768px-Solid_blue.svg.png',
+      rightAnswer:"Blau"
+   
+    },{
+       "id":3,
+    word:"green",
+      options:['Pink','Blau','Grün','Rot'],
+      img:'https://www.or-taas.co.il/wp-content/uploads/2014/02/green-03.jpg',
+      rightAnswer:"Grün"
+   
+   },
+   
+   {
+       "id":4,
+       word:"yellow",
+         options:['Pink','Gelb','Grün','schwarz'],
+         img:'https://lh3.googleusercontent.com/AyILvdXo4xjOh8jvLSgeUgCDr20wYZ-1LdGv4qHl5CfpRi7QfHNcLNL-n62fbd6qyA',
+         rightAnswer:"Gelb"
+      
+      },
+      {
+       "id":5,
+       word:"black",
+         options:['Pink','Gelb','Grün','schwarz'],
+         img:'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAA1BMVEUAAACnej3aAAAASElEQVR4nO3BgQAAAADDoPlTX+AIVQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDcaiAAFXD1ujAAAAAElFTkSuQmCC',
+         rightAnswer:"schwarz"
+      
+      },
+      {
+       "id":6,
+       word:"yellow",
+         options:['Pink','Gelb','Grün','schwarz'],
+         img:'../quizz game/Game/color/yellow.png',
+         rightAnswer:"Gelb"
+      
+      },
+      {
+       "id":7,
+       word:"orange",
+         options:['Pink','Gelb','Orange','schwarz'],
+         img:'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBw0HBwcHBw0NBwcHDQ0HBwcHDQ8IDQcNIBEWFhURFRMYHSggGCYlGxUVITEhJSkrLi4uFx8zODMtNygtLisBCgoKDQ0NDg0NFS0ZFRkrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrK//AABEIALcBEwMBIgACEQEDEQH/xAAWAAEBAQAAAAAAAAAAAAAAAAAAAQP/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/8QAFQEBAQAAAAAAAAAAAAAAAAAAAAb/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwDcBCr8AAAAAAAAAAAAAAAAAAAAAAAAABFAAAAAAEBQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARUBQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEVAUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEBQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARQAABBQAAAQBRAFAAAAAAAAEAUQBQABAFAAEAUAAAAQBRAFEAVAB//9k=',
+         rightAnswer:"Orange"
+      
+      },
+      {
+       "id":8,
+       word:"pink",
+         options:['Rosa','lila','rot','weiß'],
+         img:'https://images.fabric.com/images/605/605/AP-895.jpg',
+         rightAnswer:"Rosad"
+      
+      },
+      {
+       "id":9,
+       word:"white",
+         options:['Rosa','lila','rot','weiß'],
+         img:'https://www.whitehotel.com/files/img/bg-lines.png',
+         rightAnswer:"weiß"
+      
+      },
+      {
+       "id":10,
+       word:"purple",
+         options:['Rosa','lila','rot','weiß'],
+         img:'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAA1BMVEVmAJli86GoAAAASElEQVR4nO3BgQAAAADDoPlTX+AIVQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDcaiAAFXD1ujAAAAAElFTkSuQmCC',
+         rightAnswer:"lila"
+      
+      }
+   
+   ]
+   
+   
+   
+   
+   const Animals=[
+   
+   {
+   
+       "id":1,
+       word:"snake",
+       options:['Frosch', 'Schlange', 'warum', 'Elefant'],
+       img:'https://i.insider.com/5ba126df3cccd120008b4568?width=1100&format=jpeg&auto=webp',
+       rightAnswer:"Schlange"
+   
+   },
+   {
+       "id":2,
+       word:"frog",
+       options:['Frosch', 'Schlange', 'warum', 'Elefant'],
+       img:'https://static.scientificamerican.com/sciam/cache/file/41DF7DA0-EE58-4259-AA815A390FB37C55_source.jpg',
+       rightAnswer:"Frosch"
+   
+   },
+   {
+       "id":3,
+       word:"elephant",
+       options:['Frosch', 'Schlange', 'warum', 'Elefant'],
+       img:'https://www.iucn.org/sites/dev/files/content/images/2020/img_8130_edited.jpg',
+       rightAnswer:"Elefant"
+   
+   },
+   {
+       "id":4,
+       word:"Lama",
+       options:['Frosch', 'Schlange', 'Lama', 'Elefant'],
+       img:'https://www.schladming-dachstein.at/A-Z_Liste/Haus-Aich-Goessenberg/Ausflugsziele/Alpaca-Lamawelt%20KLAUSNERHOF/Lama%20und%20Alpaka_%C2%A9Klausnerhof%20%281%29.JPG',
+       rightAnswer:"Lama"
+   
+   },{
+       "id":5,
+   word:"dog",
+   options:['Hund', 'Katze', 'Pferd', 'Tiger'],
+   img:'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=640:*',
+   rightAnswer:"Hund"
+   
+   },
+   {
+       "id":6,
+   word:"cat",
+   options:['Hund', 'Katze', 'Pferd', 'Tiger'],
+   img:'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg',
+   rightAnswer:"Katze"
+   
+   },
+   {
+       "id":7,
+   word:"horse",
+   options:['Hund', 'Katze', 'Pferd', 'Tiger'],
+   img:'https://www.thesprucepets.com/thmb/MZcIufmv4DQ6QlSYy6YBmo0Pzg4=/3435x2576/smart/filters:no_upscale()/horse-galloping-in-grass-688899769-587673275f9b584db3a44cdf.jpg',
+   rightAnswer:"Pferd"
+   
+   },
+   {
+       "id":8,
+   word:"tiger",
+   options:['Hund', 'Katze', 'Pferd', 'Tiger'],
+   img:'https://c402277.ssl.cf1.rackcdn.com/photos/18134/images/priority_species_small/Medium_WW226365.jpg?1574452099',
+   rightAnswer:"Tiger"
+   
+   },{
+       "id":9,
+   word:"camel",
+   options:['Hund' ,'Löwe', 'Kamel', 'Tiger'],
+   img:'https://www.ynet.co.il/PicServer4/2015/12/08/6684509/668450501000100490326no.jpg',
+   rightAnswer:"Kamel"
+   
+   },{
+       "id":10,
+   word:"lion",
+   options:['Hund' ,'Löwe', 'Kamel', 'Tiger'],
+   img:'https://upload.wikimedia.org/wikipedia/commons/2/23/Male_lion_picture._Shot_by_Vikta_JuiceBoy.jpg',
+   rightAnswer:"Löwe"
+   }
+   
+   ]
+   
+   
+   
+   
+   const Job=[
+   
+   
+       {
+           "id":1,
+           word:"teacher",
+           options:['Lehrer', 'Dozent', 'Busfahrer', 'Taxifahrer'],
+           img:'https://static.timesofisrael.com/www/uploads/2020/05/F200517YS25.jpg',
+           rightAnswer:"Löwe"
+           },
+   
+           {
+               "id":2,
+               word:"lecturer",
+               options:['Lehrer', 'Dozent', 'Busfahrer', 'Taxifahrer'],
+               img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQpC5Q9ZgzayngkHTT6nQY_eRr1IwgEGO-HPA&usqp=CAU',
+               rightAnswer:"Dozent"
+               },
+   
+               {
+                   "id":3,
+                   word:"taxi driver",
+                   options:['Lehrer', 'Dozent', 'Busfahrer', 'Taxifahrer'],
+                   img:'https://i.pinimg.com/originals/01/ca/56/01ca56cc9550d4b3d500c6083335651b.jpg',
+                   rightAnswer:"Taxifahrer"
+                   },
+   
+   
+                   {
+                       "id":4,
+                       word:"farmer",
+                       options:['LKW-Fahrer', 'Bauer', 'Kuhhirte', 'Soldat'],
+                       img:'https://img.etimg.com/thumb/msid-65900138,width-1070,height-580,imgsize-120883,overlay-economictimes/photo.jpg',
+                       rightAnswer:"Kuhhirte"
+                       },{
+                           "id":5,
+                       word:"Truck driver",
+                       options:['LKW-Fahrer', 'Bauer', 'Kuhhirte', 'Soldat'],
+                       img:'https://www.yourdictionary.com/images/definitions/lg/4320.farmer.jpg',
+                       rightAnswer:"LKW-Fahrer"
+                       },
+               
+                       {
+                           "id":6,
+                           word:"cowherd",
+                           options:['LKW-Fahrer', 'Bauer', 'Kuhhirte', 'Soldat'],
+                           img:'https://besttv232-ynet-images1-prod.cdn.it.best-tv.com/PicServer5/2017/03/16/7654651/765462711991099980549no.jpg',
+                           rightAnswer:"Kuhhirte"
+                           },
+               
+                           {
+                               "id":7,
+                               word:"soldier",
+                               options:['LKW-Fahrer', 'Bauer', 'Kuhhirte', 'Soldat'],
+                               img:'https://www.idf.il/media/18724/whatsapp-image-2017-10-29-at-173633-3.jpeg',
+                               rightAnswer:"Soldat"
+                               },
+               
+               
+                               {
+                                   "id":8,
+                                   word:" farmer",
+                                   options:['LKW-Fahrer', 'Bauer', 'Kuhhirte', 'Soldat'],
+                                   img:'https://img.etimg.com/thumb/msid-65900138,width-1070,height-580,imgsize-120883,overlay-economictimes/photo.jpg',
+                                   rightAnswer:"Kuhhirte"
+                                   },      
+                                   {   
+                                       "id":9,
+                                   word:" DJ",
+                                   options:['LKW-Fahrer', 'Barkeeper', 'DJ', 'Soldat'],
+                                   img:'https://mit4mit.s3.amazonaws.com/uploads/biz/33948/pic/cover/coverMobile.jpg?v=3',
+                                   rightAnswer:"DJ"
+                                   }, 
+                                    {    
+                                   "id":10,
+                                   word:" bartender",
+                                   options:['LKW-Fahrer', 'Barkeeper', 'DJ', 'Soldat'],
+                                   img:'https://barmagazine.co.uk/wp-content/uploads/2019/09/kyryll-ushakov-b2pDjDp7VRM-unsplash.jpg',
+                                   rightAnswer:"Barkeeper"
+                                   },     
+   
+   
+   ]
+   
+var catagory
+var index = 0;
+var score = document.getElementById("scorecounter")
+
+function quizGame(){
+
+    catagory = document.getElementById("catagory")
+    score = document.getElementById("scorecounter")
+    var myAnswer = document.getElementsByClassName("myAnswer")
+    var picture = document.getElementById("picture")
+    switch (catagory.innerHTML) {
+        case "Color":
+            catagory = Color
+            break;
+        case "Animals":
+            catagory = Animals
+            break; 
+        case "Job":
+            catagory = Job
+            break;
+            
+    }
+    picture.src = catagory[index].img
+    var i = 0;
+    for (const answer of myAnswer) {
+        answer.value = catagory[index].options[i];
+        answer.innerHTML = catagory[index].options[i]
+        i++
+    }
+}
+
+function gameOver(){
+    alert("WIN!  Your score is: " + score.innerHTML)
+}
+
+function rightAnswer()
+{
+    console.log(score.innerHTML)
+    score.innerHTML = 1 + Number(score.innerHTML);
+    alert("Right answer!")
+    nextQustion()
+}
+
+function wrongAnswer() {
+
+    console.log(score.innerHTML)
+    score.innerHTML = Number(score.innerHTML) - 0.5;
+    alert("Wrong !please try again")
+}
+
+function nextQustion(){
+    console.log(index)
+    console.log(catagory.length)
+    index++
+    if (index < catagory.length) {
+        quizGame();
+    }
+    else{
+        gameOver();
+    }
+}
+function previousQustion(){
+    index--
+    quizGame();
+}
+
+function myAnswer(userAnswer){
+    if (userAnswer == catagory[index].rightAnswer) {
+        rightAnswer()
+    }
+    else{
+        wrongAnswer()
+    }
+    
+}
+    
